7b196b08b9d1ddf612e80ade9ed2dfadde12e56c
pedropaixaosantos1-lab <pedropaixaosantos1@gmail.com>
2026-02-27 13:48:27 -0300

    aula css

--- src/app/page.js
+++ src/app/page.js
@@ -4,18 +4,13 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div>
-      <h1>React com Nest JS</h1>
+      <h1>React com Next JS</h1>
+      <h2>Aula de introdução</h2>
+      <h3>leiaute e Estilização</h3>
+      <p>Nesta aula iremos entender iremos entender o uso de CSS global e local.</p>
     </div>
   );
 }
  
-const Home = () => {
-   return (
-    <div>
-      <h3>Aula introdução</h3>
-      <h3>Aula estilização</h3>
-      <h3>Aula Componentes</h3>
-    </div>
-  );
-}
+
 
