4d13a5d5fbe5a16a42fbdc632d00da2db21d2c5c
pedropaixaosantos1-lab <pedropaixaosantos1@gmail.com>
2026-03-16 15:22:59 -0300

    aula atv 01

--- src/app/atividades/atv-01/page.jsx
+++ src/app/atividades/atv-01/page.jsx
@@ -1,12 +1,13 @@
-import link from "next/link";
-
-import styles from "/page.module.css";
+import Link from "next/link";
+import styles from "./page.module.css";
 function Atividade01 () {
     return(
-        <div>
+        <div class = {styles.tituloAtv}>
             <h1>Atividades 1</h1>
             <p>Componente basico para renderizar tela com react</p>
-            <Link href="/exemplos/ex-01">Atividade 1 </Link>
+           
+
+
         </div>
     )
 }
---
+++ src/app/atividades/atv-02/page.jsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+import styles from "./page.module.css";
+function Atividade02 () {
+    return(
+        <div>
+            <h1>Atividades 2</h1>
+            <p>Componente basico para renderizar tela com react</p>
+           
+
+
+        </div>
+    )
+}
+export default Atividade02;
