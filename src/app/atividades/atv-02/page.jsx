321019ec96a5b1ce5785ba5795d0ba88c3ac8b4b
pedropaixaosantos1-lab <pedropaixaosantos1@gmail.com>
2026-03-23 15:24:20 -0300

    atv-02

--- src/app/atividades/atv-02/page.jsx
+++ src/app/atividades/atv-02/page.jsx
@@ -1,15 +1,39 @@
-import Link from "next/link";
+'use client'
 
-import styles from "./page.module.css";
-function Atividade02 () {
-    return(
-        <div>
-            <h1>Atividades 2</h1>
-            <p>Componente basico para renderizar tela com react</p>
-           
+import { useState } from "react";
 
+import styles from './page.module.css';
 
+export default function Atividade2() {  
+   
+    const [num, setNum] = useState(100);
+
+    function handleIncrementa () {
+        setNum(num + 1);
+    }
+    function handleDecrementa () {
+        setNum(num - 1);
+    }
+
+    return (
+        <div className={styles.container}>
+
+        <h1 className={styles.titulo}>Atividade 2</h1>
+
+   <p className={styles.texto1}>Contador incrementando e decrementando com uso de useState do React</p>
+
+       <div className={styles.container1}>
+            <label 
+                onClick={() => handleIncrementa()}
+                className={styles.botao}
+            >+1</label>
+            <label className={styles.texto}>{`Contador: ${num}`}</label>            
+
+            <label 
+                onClick={() => handleDecrementa()}
+                className={styles.botao}
+            >-1</label>
+            </div>
         </div>
-    )
+    );
 }
-export default Atividade02;
--- src/app/exemplos/ex-03/page.jsx
+++ src/app/exemplos/ex-03/page.jsx
@@ -1,8 +1,8 @@
 'use client'
 
-import styles from "./page.module.css";
 import { useState } from "react";
 
+import styles from './page.module.css';
 
 export default function Exemplo03() {  
 
@@ -13,11 +13,12 @@ export default function Exemplo03() {
     }
 
     return (
-        
-       <div className={styles.labelcss}>
-            <label>{`Contador: ${num}`}</label>            
-            <button onClick={() => handleIncrementa()}className={styles.adicioarnumero}>+1</button>
-            </div>
-    
-    );  
+        <div className={styles.container}>
+            <label className={styles.texto}>{`Contador: ${num}`}</label>            
+            <label 
+                onClick={() => handleIncrementa()}
+                className={styles.botao}
+            >+1</label>
+        </div>
+    );
 }
